243ba92e40f72b66488793b4d1056fbfe153aae0
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-17 16:46:44 +0530

    16/07

--- Rock_Paper_Scissor/app.js
+++ Rock_Paper_Scissor/app.js
@@ -1,1 +1,49 @@
 //here write logic
+let divs = document.querySelectorAll(".image-box");
+let userScore = document.querySelector("#user-score")
+let compScore = document.querySelector("#comp-score")
+let result = document.querySelector("#result")
+
+divs.forEach((div) => {
+    div.addEventListener("click", (evt) => {
+        let userInput = evt.currentTarget.id;
+        console.log("userInput = ", userInput)
+        let comInput = getCompInput();
+        console.log("compInput = ", comInput)
+        let winner = getWinner(userInput, comInput)
+        console.log(winner);
+        if (winner == "Match is draw") {
+            result.innerHTML = winner;
+            result.style.backgroundColor = "blue";
+        }
+        else if (winner == "Computer winner!") {
+            result.innerHTML = winner;
+            result.style.backgroundColor = "red";
+            compScore.innerHTML = Number(compScore.innerHTML) + 1;
+        }
+        else {
+            result.innerHTML = winner;
+            result.style.backgroundColor = "green";
+            userScore.innerHTML = Number(userScore.innerHTML) + 1;
+        }
+
+    })
+})
+
+function getCompInput() {
+    let list = ["rock", "paper", "scissor"]
+    let index = parseInt(Math.random() * 3);
+    return list[index];
+}
+
+function getWinner(userInput, comInput) {
+    if (userInput == comInput) {
+        return "Match is draw";
+    }
+    else if (userInput == "rock" && comInput == "paper" || userInput == "paper" && comInput == "scissor" || userInput == "scissor" && comInput == "rock") {
+        return "Computer winner!";
+    }
+    else {
+        return "User winner!";
+    }
+}
