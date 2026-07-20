500de11db26b5973d6d2283e4f78429d2092cc72
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-20 17:13:24 +0530

    07/07

--- Stopwatch project/app.js
+++ Stopwatch project/app.js
@@ -1,14 +1,65 @@
 let timer = document.querySelector(".timer")
-let stop = document.querySelector("#stop")
+let reset = document.querySelector("#reset")
 let start = document.querySelector("#start")
+let lap = document.querySelector("#lap")
 
 let mins = 0;
 let secs = 0;
 let msecs = 0;
+let id = null
+let div = false;
+let divBox = null;
 
 start.onclick = () => {
-    setInterval(startTimer, 10)
+    if (id == null) {
+        id = setInterval(startTimer, 10)
+        start.innerHTML = "Stop";
+        start.style.backgroundColor = "red";
+        reset.style.visibility = "visible";
+        lap.style.visibility = "visible";
+    }
+    else {
+        clearInterval(id);
+        id = null;
+        start.innerHTML = "Start";
+        start.style.backgroundColor = "green";
+    }
 }
+
+reset.onclick = () => {
+    timer.innerHTML = "00 : 00 : 00";
+    msecs = secs = mins = 0;
+    clearInterval(id);
+    id = null;
+    start.innerHTML = "Start";
+    start.style.backgroundColor = "green";
+    reset.style.visibility = "hidden";
+    reset.style.visibility = "hidden";
+    lap.style.visibility = "hidden";
+    divBox.remove();
+    div = false;
+}
+
+lap.onclick = () => {
+    if (div == false) {
+        divBox = document.createElement("div");
+        let p = document.createElement("p");
+        let h1 = document.createElement("h1");
+        h1.innerText = "timer Lap";
+        divBox.classList.add("div-box");
+        p.innerHTML = timer.innerHTML;
+        div = true;
+        divBox.append(h1);
+        divBox.append(p);
+        document.body.append(divBox);
+    }
+    else {
+        let p = document.createElement("p");
+        p.innerHTML = timer.innerHTML;
+        divBox.append(p);
+    }
+}
+
 function startTimer() {
     msecs++;
     if (msecs == 100) {
