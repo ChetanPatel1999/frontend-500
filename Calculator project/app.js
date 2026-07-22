0e53a96db3f60d6e659167fcabe3dd6eb39709fc
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-22 17:03:49 +0530

    07/07

--- Calculator project/app.js
+++ Calculator project/app.js
@@ -1,28 +1,60 @@
-let btn = document.querySelectorAll(".btn");
+let btns = document.querySelectorAll(".btn");
 let inputBox = document.querySelector("#input-box");
 
 let str = "";
-btn.forEach((bt) => {
-    bt.addEventListener("click", (evt) => {
-        let inputValue = evt.target.innerText;
 
-        if (inputValue == "AC") {
-            str = "";
-            inputBox.value = str;
-        }
-        else if (inputValue == "DEL") {
-               
-        }
-        else if (inputValue == "=") {
-            str = eval(str);
-            console.log(typeof str)
-            inputBox.value = str;
-        }
-        else {
+
+const btnHandler = (inputValue) => {
+
+
+    if (inputValue == "AC") {
+        str = "";
+        inputBox.value = str;
+    }
+    else if (inputValue == "DEL")// "3+5+9"
+    {
+        str = str + ""
+        str = str.substring(0, str.length - 1)
+        inputBox.value = str;
+    }
+    else if (inputValue == "=") {
+        str = eval(str);
+        inputBox.value = str;
+    }
+    else {
+        if (str.length > 0) {
+
+            if ("+/%*-.".includes(str[str.length - 1]) && "+/%*-.".includes(inputValue)) {
+                let newstr = ""
+                for (let i = 0; i < str.length; i++) {
+                    if (i == str.length - 1) {
+                        newstr = newstr + inputValue;
+                    }
+                    else {
+                        newstr = newstr + str[i];
+                    }
+                }
+                console.log(newstr)
+                str = newstr;
+                inputBox.value = str;
+            }
+            else {
+                str = str + inputValue;
+                inputBox.value = str;
+            }
+        } else {
             str = str + inputValue;
             inputBox.value = str;
         }
+    }
 
 
+}
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (evt) => {
+        let inputValue = evt.target.innerText;
+        btnHandler(inputValue);
     })
 })
+
