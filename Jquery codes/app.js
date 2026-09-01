e5c81b8329c78c7e007fb4c93f62482c5504e3e7
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-01 17:02:08 +0530

    14/08

---
+++ Jquery codes/app.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () {
+//     //here we write jquery code
+//     $("#btn").click(() => {
+//         console.log("hello jquery")
+//     })
+// })
+
+
+// $(function () {
+//     //here we write jquery code
+//     $("#btn").click(() => {
+//         console.log("hello jquery")
+//     })
+// })
+
+$(document).ready(function () {
+    $("#btn").click(function () {
+        console.log("hello jquery")
+    });
+});
